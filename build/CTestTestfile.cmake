5144cad76e1ba0ca3d4e9e86616640840ab00611
Ethan Terrero <ethan.terrero@gmail.com>
2026-01-29 17:35:36 -0800

    Stop tracking build artifacts

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ethanterrero/Desktop/crypt-analysis
-# Build directory: /Users/ethanterrero/Desktop/crypt-analysis/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
